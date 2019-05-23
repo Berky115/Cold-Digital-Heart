6d6fb1df8dfa5186fe0fe70d49bc926c98253a53
Drew Facchiano <drewfacchiano@Drews-MacBook-Pro.local>
2019-05-23 11:50:09 -0700

    Game Component and Blog
    -Restored game componenet functionality
    - Updated blog format

--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import email from './resources/contact/email.png';
+import linkedin from './resources/contact/linkedin.png';
+import lodash from 'lodash';
+import {blogData} from './BlogData';
+import GameComponent from './GameComponent';
+const _ = lodash;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class About extends React.Component {
+// <img src={email} className="App-logo" alt="logo" />
+  render() {
+    return (
+      <div>
+        <ul>
+          <li className="aboutMe">
+            Andrew (Drew) is a studied software developer currently living in the Bay Area. He enjoys long walks in the park, riding bikes and working song lyrics
+            into conversations. He has worked on corporate software, websites and video games. He enjoys teaching and learning new skills.
+          </li>
+      </ul>
     </div>
+    );
+  }
+}
+
+class Projects extends React.Component {
+  render() {
+    return (
+      <div>
+         <ul>
+             <li className="project"><a href="audioInvaders/index.html"><h1>Audio Invaders</h1></a></li>
+             <li className="project"><a href="https://berkyland.wordpress.com/author/berkyland/"><h1>Berkyland Blog (old)</h1></a></li>
+             <li className="project"><a href="http://www.lensthegame.com/"><h1>Lens</h1></a></li>
+             <li className="project"><a href="http://www.adventuretimegamejam.com/submissions/62-the-pit-of-the-ice-king"><h1>Pit of The Ice King</h1></a></li>
+             <li className="project"><a href="js/datcat/GGJ_Builds.html"><h1>Dat Cat</h1></a></li>
+             <li className="project"><a href="https://nitwic.podbean.com"><h1>Nitwic!</h1></a></li>
+       </ul>
+      </div>
+    );
+  }
+}
+
+class Blog extends React.Component {
+  render() {
+    let blogEntries = blogData.postsArray.map(entry =>
+      <li key={entry + Math.random()}className="blogEntry">
+        <div className='flexcontainer'>
+          <div className="flex-item blogDate"> {entry.datePosted} </div>
+        </div>
+        <div className="titleText">{entry.postTitle}</div>
+        
+        <br></br>
+        {
+          entry.postText.map(line =>
+            <div key={line + Math.random()} className="blogText"> {line}
+            <br></br>
+            <br></br>
+            </div> )
+        }
+      </li>
+    );
+
+      return (
+        <div>
+          <ul> 
+            {blogEntries}
+          </ul>
+        </div>
+      );
+  }
+}
+
+class Contact extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1> Andrew Facchiano </h1>
+        <div>
+          <a href="mailto:andrewfacchiano@gmail.com" >
+            <img alt='e-mail' src={email} className="contactNode"></img><p> By E-mail at andrewfacchiano@gmail.com </p>
+          </a>
+        </div>
+
+        <div>
+          <a href="https://www.linkedin.com/in/andrew-facchiano-0961a761">
+            <img alt='linked in' src={linkedin} className="contactNode"></img><p> On Linkdin </p>
+          </a>
+        </div>
+    </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDisplay: 'Home'
+    }
+  }
+
+  handleClick = function(event){
+    this.setState({
+      currentDisplay: event.currentTarget.textContent,
+    }); 
+  }
+  
+  render() {
+    const current = this.state.currentDisplay;
+    return (
+      <div>
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css?family=Architects+Daughter|Coming+Soon|Gloria+Hallelujah" rel="stylesheet"></link>
+          <GameComponent/>
+            <ul className = "header" >
+                <li onClick={(i) => this.handleClick(i)}>Home</li>
+                <li onClick={(i) => this.handleClick(i)}>Blog</li>
+                <li onClick={(i) => this.handleClick(i)}>Projects</li>
+                <li onClick={(i) => this.handleClick(i)}>About</li>
+                <li onClick={(i) => this.handleClick(i)}>Contact</li>
+            </ul>
+            {current === 'Contact' && <Contact/>}
+            {current === 'Blog' && <Blog/>}
+            {current === 'Projects' && <Projects/>}
+            {current === 'About' && <About/>}
+            <div className="content">
+            {this.props.children}
+            </div>
+      </div>
   );
+  }
 }
 
 export default App;
---
+++ src/GameComponent.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import _ from 'lodash';
+
+function Invader(speed, radius, positionX, positionY, noteColor) {
+  this.speed = speed;
+  this.radius = radius;
+  this.positionX = positionX;
+  this.positionY = positionY;
+  this.noteColor = noteColor;
+  this.detectedState = false;
+}
+
+
+class GameComponent extends React.Component {
+  myRef = React.createRef();
+  state = {
+    //numberOfInvaders, minInvaderSize, maxInvaderSize, minSpeed, maxSpeed, BGMusic
+    gameSettings: {
+      numberOfInvaders:5,
+      minInvaderSize:10,
+      maxInvaderSize:15,
+      minSpeed:1,
+      maxSpeed:4,
+    },
+    WIDTH: window.screen.availWidth,
+    HEIGHT: 100,
+    colors: [
+        "red",
+        "#aaa",
+        "black"
+    ],
+    enemies:[],
+  };
+
+
+  randomNumber(min, max) {
+    return Math.floor(Math.random() * max + min);
+  }
+
+drawCircle(ctx, x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, true);
+  ctx.fill();
+}
+
+drawInvader(ctx, invader) {
+    ctx.fillStyle = invader.noteColor;
+    this.drawCircle(ctx, invader.positionX, invader.positionY, invader.radius);
+    ctx.fillStyle = "white";
+    this.drawCircle(ctx, invader.positionX - invader.radius / 2, invader.positionY, (invader.radius) / 4);
+    this.drawCircle(ctx, invader.positionX + invader.radius / 2, invader.positionY, (invader.radius) / 4);
+}
+
+clear(ctx) {
+        ctx.clearRect(0, 0, this.state.WIDTH, this.state.HEIGHT);
+}
+
+initGame(){
+  //https://stackoverflow.com/questions/29537299/react-how-do-i-update-state-item1-on-setstate-with-jsfiddle
+  let newState = _.cloneDeep(this.state);
+  for (let i = 0; i < newState.gameSettings.numberOfInvaders; i++) {
+    // 1. Make a shallow copy of the items
+    let newEnemies = [...newState.enemies];
+    // 2. Make a shallow copy of the item you want to mutate
+    let enemy = {...newEnemies[i]};
+    // 3. Replace the property you're intested in
+    // speed, radius, positionX, positionY, noteColor
+    enemy = new Invader( 
+      this.randomNumber(this.state.gameSettings.minSpeed, this.state.gameSettings.maxSpeed),
+      this.randomNumber(this.state.gameSettings.minInvaderSize, this.state.gameSettings.maxInvaderSize), 
+      this.randomNumber(this.state.WIDTH, this.state.WIDTH + 20), 
+      this.randomNumber(this.state.gameSettings.maxInvaderSize, this.state.HEIGHT - 35), 
+      this.state.colors[this.randomNumber(0,this.state.colors.length)]
+      );
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+    newEnemies[i] = enemy;
+    newState.enemies = newEnemies;
+    // 5. Set the state to our new copy
+    this.setState(newState);
+  }
+}
+
+draw(ctx) {
+    if(ctx) {
+      this.clear(ctx);
+      for (let i = 0; i < this.state.enemies.length; i++) {
+          this.drawInvader(ctx,this.state.enemies[i]);
+          if (this.state.enemies[i].positionX > -50) {
+            let newEnemies = this.state.enemies;
+            newEnemies[i].positionX -= this.state.enemies[i].speed;
+            this.setState({
+              enemies: newEnemies,
+            });
+            // this.state.enemies[i].positionX -= this.state.enemies[i].speed;
+          } else {
+            let newEnemies = this.state.enemies;
+            newEnemies[i] = this.resetEnemy(this.state.enemies[i]);
+            this.setState({
+              enemies: newEnemies,
+            });
+            // this.state.enemies[i] = this.resetEnemy(this.state.enemies[i]);
+          }
+      }
+    }
+};
+
+resetEnemy(invader) {
+    invader.positionX = this.randomNumber(this.state.WIDTH, this.state.WIDTH + 20);
+    invader.positionY = this.randomNumber(this.state.gameSettings.maxInvaderSize, this.state.HEIGHT - 35);
+    invader.speed = this.randomNumber(this.state.gameSettings.minSpeed, this.state.gameSettings.maxSpeed);
+    invader.radius = this.randomNumber(this.state.gameSettings.minInvaderSize, this.state.gameSettings.maxInvaderSize);
+    invader.noteColor = this.state.colors[this.randomNumber(0,this.state.colors.length)];
+    return invader;
+};
+
+componentDidMount() {
+  const ctx = this.refs.canvas.getContext("2d");
+  setInterval(this.draw.bind(this,ctx), 16.67); //approx 60 fps
+  window.onload = this.initGame();
+}
+
+render() {
+    return (
+      <div>
+        <canvas ref="canvas" id="canvas" width={this.state.WIDTH} height={this.state.HEIGHT} ></canvas>
+      </div>
+    );
+  }
+}
+
+export default GameComponent;
