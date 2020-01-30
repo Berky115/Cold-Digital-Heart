87f75b8ead5703f2b1d51e753261e64cb5ad19cc
Drew Facchiano <andrewfacchiano@berkeley.edu>
2020-01-29 16:38:45 -0800

    production ready build

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Footer from './Footer';
 
 ReactDOM.render(<App  />, document.getElementById('Heart'));
-// ReactDOM.render(<Footer />, document.getElementById('Footer'));
 
